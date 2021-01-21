d982e513f478613bece5068126e6d50f9686806f
Suraj-92 <suraj.temkar.92@gmail.com>
2021-01-21 16:26:27 +0530

    Using Switch case done

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,19 +1,23 @@
 public class HelloWorld{
+
+	 public static final int isPartTime=1;
+      public static final int isFullTime=2;
+      public static final int empRatePerHr=20;
+
 	public static void main(String[] args){
-		int isPartTime=1;	
-		int isFullTime=2;
-		int empRatePerHr=20;
 	int empHrs=0;
 	int empWage=0;
-		double empCheck=Math.floor(Math.random() * 10) % 3;
-		
-		if( empCheck == isPartTime )
-			empHrs=4;
-		else if(empCheck==isFullTime)
-			empHrs=8;
-		
-		else		
-			empHrs=0;
+		int empCheck=(int) Math.floor(Math.random() * 10) % 3;
+		switch (empCheck){
+			case isPartTime:
+							empHrs=4;
+						break;
+			case isFullTime:
+							empHrs=8;
+						break;
+			default:
+					empHrs=0;
+	}
 			empWage=empHrs * empRatePerHr;
 			System.out.println("Employee Wage: "+empWage);
 	}
